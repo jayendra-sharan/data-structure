1b01adaa18fc1b6868813f02b47dcaa59298999e
Jayendra <jayendra.sharan@gmail.com>
2018-09-22 11:43:09 +0530

    Updated stack implementation.

--- stack.js
+++ stack.js
@@ -1,27 +1,64 @@
-function Stack () {
+function Stack (N) {
   this.stack = [];
+  this.size = N;
 }
 
+/**
+ * @description push an element in the stack.
+ * @param {any} data to be inserted.
+ */
 Stack.prototype.push = function (data) {
-  this.stack.push (data);
+  if (!this.isFull ()) {
+    this.stack.push (data);
+  } else {
+    console.error ("Stack is full.");
+  }
 }
 
-Stack.prototype.pop = function (data) {
-  return this.stack.pop ();
+/**
+ * @description removes and returns the element at the top.
+ */
+Stack.prototype.pop = function () {
+  if (!this.isEmpty ()) {
+    return this.stack.pop ();
+  } else {
+    return "Stack is empty.";
+  }
 }
 
-Stack.prototype.length = function () {
-  return this.stack.length;
+/**
+ * @description checks if the stack is empty.
+ */
+Stack.prototype.isEmpty = function () {
+  return !this.stack.length
 }
 
-Stack.prototype.top = function () {
-  return this.stack [this.stack.length - 1]
+/**
+ * @description checks if the stack is full.
+ */
+Stack.prototype.isFull = function () {
+  return this.stack.length === this.size;
 }
 
-var st = new Stack ();
+/**
+ * @description returns the element at the top. Does not remove it.
+ */
+Stack.prototype.peek = function () {
+  if (!this.isEmpty ()) {
+    return this.stack [this.stack.length - 1];
+  } else {
+    return "Stack is empty."
+  }
+}
+
+var st = new Stack (2);
+console.log(st.isFull ());
+console.log(st.isEmpty());
+console.log (st.pop ());
 st.push (10);
 st.push (20);
-st.push (30);
-console.log (st.top ());    // 30
-console.log (st.pop ());    // 30
-console.log (st.top ());    // 20
+console.log (st.peek ());
+console.log (st.pop ());
+console.log (st.pop ());
+console.log (st.pop ());
+
